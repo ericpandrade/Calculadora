eee593e59c7223282c9df951ad04211645703436
Eric Pereira Andrade <ericpandrade085@gmail.com>
2021-08-23 18:26:31 -0300

    Final

---
+++ script.js
@@ -0,0 +1,29 @@
+function insert(value) {
+  const concatenation = document.getElementById("answer").value;
+  document.getElementById("answer").value = concatenation + value;
+}
+
+function clean() {
+  document.getElementById("answer").value = "";
+}
+
+function back() {
+  const answer = document.getElementById("answer").value;
+  document.getElementById("answer").value = answer.substring(
+    0,
+    answer.length - 1
+  );
+}
+
+function calcular() {
+  let answer = document.getElementById("answer").value;
+  try {
+    if (answer) {
+      document.getElementById("answer").value = eval(answer);
+    } else {
+      document.getElementById("answer").value = "Nothing to calculate.";
+    }
+  } catch {
+    document.getElementById("answer").value = "Something wrong... Try again";
+  }
+}
